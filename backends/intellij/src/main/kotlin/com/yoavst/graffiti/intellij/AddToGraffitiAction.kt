573d689f507a5ac7e103a20055ce32102e1a355e
Yoav Sternberg <yoav.sternberg@gmail.com>
2023-12-15 00:29:34 +0200

    :sparkles: Line node for intellij

--- backends/intellij/src/main/kotlin/com/yoavst/graffiti/intellij/AddToGraffitiAction.kt
+++ backends/intellij/src/main/kotlin/com/yoavst/graffiti/intellij/AddToGraffitiAction.kt
@@ -1,27 +1,17 @@
 package com.yoavst.graffiti.intellij
 
-import com.google.gson.Gson
 import com.intellij.codeInsight.documentation.DocumentationManager
-import com.intellij.lang.documentation.DocumentationProvider
-import com.intellij.lang.documentation.ide.IdeDocumentationTargetProvider
-import com.intellij.notification.NotificationGroupManager
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiField
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiJavaFile
-import com.intellij.psi.PsiMethod
+import com.intellij.psi.*
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
+import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 
 
 open class AddToGraffitiAction : AnAction() {
@@ -59,19 +49,19 @@ open class AddToGraffitiAction : AnAction() {
         }
 
         if (update != null) {
-            sendUpdate(event.project!!, update)
+            SocketHolder.sendUpdate(event.project!!, update)
         }
     }
 
     private fun createKotlinMethodUpdate(project: Project, psiFile: PsiFile, method: KtNamedFunction): MutableMap<String, Any> {
-        val className = method.containingClass()?.name ?: psiFile.name
+        val className = method.containingClassOrObject?.name ?: psiFile.name
         val methodName = method.name ?: "<anonymous>"
         val address = psiFile.originalFile.virtualFile.path + "@" + method.textOffset
         return createMethodUpdate(project, className, methodName, address, method)
     }
 
     private fun createKotlinFieldUpdate(project: Project, psiFile: PsiFile, field: KtProperty): MutableMap<String, Any> {
-        val className = field.containingClass()?.name ?: psiFile.name
+        val className = field.containingClassOrObject?.name ?: psiFile.name
         val fieldName = field.name!!
         val address = psiFile.originalFile.virtualFile.path + "@" + field.textOffset
         return createFieldUpdate(project, className, fieldName, address, field)
@@ -137,24 +127,6 @@ open class AddToGraffitiAction : AnAction() {
                 )
             )
         ).addDocumentation(element)
-    }
-
-    private fun sendUpdate(project: Project, data: Any) {
-        val socket = SocketHolder.socket ?: run {
-            NotificationGroupManager.getInstance()
-                .getNotificationGroup("Graffiti Notifications")
-                .createNotification("Graffiti: Not connected to server", NotificationType.ERROR)
-                .notify(project)
-            return
-        }
-        socket.getOutputStream().buffered().let { stream ->
-            val json = Gson().toJson(data)
-            // write length
-            stream.write(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(json.length).array())
-            // write data
-            stream.write(json.toByteArray())
-            stream.flush()
-        }
     }
 
     private fun MutableMap<String, Any>.addDocumentation(psiElement: PsiElement): MutableMap<String, Any> {
--- backends/intellij/src/main/kotlin/com/yoavst/graffiti/intellij/SocketHolder.kt
+++ backends/intellij/src/main/kotlin/com/yoavst/graffiti/intellij/SocketHolder.kt
@@ -1,7 +1,31 @@
 package com.yoavst.graffiti.intellij
 
+import com.google.gson.Gson
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
 import java.net.Socket
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 object SocketHolder {
     var socket: Socket? = null
+
+    fun sendUpdate(project: Project, data: Any) {
+        val socket = socket ?: run {
+            NotificationGroupManager.getInstance()
+                .getNotificationGroup("Graffiti Notifications")
+                .createNotification("Graffiti: Not connected to server", NotificationType.ERROR)
+                .notify(project)
+            return
+        }
+        socket.getOutputStream().buffered().let { stream ->
+            val json = Gson().toJson(data)
+            // write length
+            stream.write(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(json.length).array())
+            // write data
+            stream.write(json.toByteArray())
+            stream.flush()
+        }
+    }
 }
