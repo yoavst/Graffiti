081558a82fce9e79b449353c7fd333256bf07fbf
Yoav Sternberg <yoav.sternberg@gmail.com>
2023-12-22 14:03:29 +0200

    :sparkles: Add xrefs, line nodes to arbitrary address

--- backends/ida/graffiti.py
+++ backends/ida/graffiti.py
@@ -9,6 +9,9 @@ import idaapi
 import ida_kernwin
 import ida_nalt
 import idc
+import idautils
+import ida_funcs
+import ida_segment
 
 def to_bytes(s):
     if sys.version_info.major == 3:
@@ -36,50 +39,55 @@ def lengthy_send(sock, data):
 sock = None
 
 class AddToGraphHandler(idaapi.action_handler_t):
-    def __init__(self):
+    def __init__(self, force_func=True):
         idaapi.action_handler_t.__init__(self)
+        self.force_func = force_func
 
     # Say hello when invoked.
     def activate(self, ctx):
         global sock
-        if ctx.cur_func:
+        if ctx.cur_func or not self.force_func:
             my_sock = sock
             if my_sock is not None:
                 payload = self.create_payload(ctx)
-                data = to_bytes(json.dumps(payload))
-                lengthy_send(my_sock, data)
+                if payload:
+                    data = to_bytes(json.dumps(payload))
+                    lengthy_send(my_sock, data)
+                else:
+                    print("Graffiti: Empty payload")
             else:
                 print("Graffiti: Not connected to server")
         else:
-            print("Not currently in a function.")
+            print("Graffiti: Not currently in a function.")
 
         return True
     
     def create_payload(self, ctx):
-         # Get the name of the current function
-        current_func_name = idc.get_func_name(ctx.cur_func.start_ea)
-        demangled_func_name = idc.demangle_name(current_func_name, idc.INF_SHORT_DN)
-        func_name = demangled_func_name or current_func_name
+        if ctx.cur_func:
+            # Get the name of the current function
+            current_func_name = idc.get_func_name(ctx.cur_func.start_ea)
+            demangled_func_name = idc.demangle_name(current_func_name, idc.INF_SHORT_DN)
+            func_name = demangled_func_name or current_func_name
 
-        # Get the name of the current IDA database
-        db_path = ida_nalt.get_input_file_path()
-        db_filename = os.path.basename(db_path)
+            # Get the name of the current IDA database
+            db_path = ida_nalt.get_input_file_path()
+            db_filename = os.path.basename(db_path)
 
-        return {
-                "type": "addData", "node": {
-                    "project": "IDA: " + db_filename,
-                    "address": str(ctx.cur_func.start_ea), 
-                    "function": func_name,
-                    "functionAddress": str(ctx.cur_func.start_ea),
-                    "computedProperties": [
-                        {
-                            "name": "label",
-                            "format": "{0}",
-                            "replacements": ["function"]
-                        }
-                    ]
-                }
-        }
+            return {
+                    "type": "addData", "node": {
+                        "project": "IDA: " + db_filename,
+                        "address": str(ctx.cur_func.start_ea), 
+                        "baseName": func_name,
+                        "baseAddress": str(ctx.cur_func.start_ea),
+                        "computedProperties": [
+                            {
+                                "name": "label",
+                                "format": "{0}",
+                                "replacements": ["baseName"]
+                            }
+                        ]
+                    }
+            }
     # This action is always available.
     def update(self, ctx):
         return idaapi.AST_ENABLE_ALWAYS
@@ -93,18 +101,110 @@ class AddToGraphWithEdgeInfoHandler(AddToGraphHandler):
         return base
     
 class AddToGraphLineHandler(AddToGraphHandler):
+    def __init__(self):
+        AddToGraphHandler.__init__(self, False)
+
     def create_payload(self, ctx):
         base = AddToGraphHandler.create_payload(self, ctx)
-        base['node']['computedProperties'] = [
+        if base:
+            base['node']['computedProperties'] = [
+                {
+                    "name": "label",
+                    "format": "{0}+{1}",
+                    "replacements": ["baseName", "line"]
+                }
+            ]
+            base['node']['address'] = str(ctx.cur_ea)
+            base['node']['line'] = '{0:x}'.format(ctx.cur_ea - ctx.cur_func.start_ea)
+            return base
+        else:
+            # Non-function address
+            db_path = ida_nalt.get_input_file_path()
+            db_filename = os.path.basename(db_path)
+
+            return {"type": "addData", "node": raw_node_from_ea(ctx.cur_ea, db_filename)}
+    
+class AddXrefsHandler(AddToGraphHandler):
+    def __init__(self, line_xrefs=False):
+        AddToGraphHandler.__init__(self)
+        self.line_xrefs = line_xrefs
+
+    def create_payload(self, ctx):
+        db_path = ida_nalt.get_input_file_path()
+        db_filename = os.path.basename(db_path)
+
+
+        nodes = []
+        for xref in idautils.XrefsTo(ctx.cur_func.start_ea):
+            f = ida_funcs.get_func(xref.frm)
+            if f is None:
+                # Raw address
+                nodes.append(raw_node_from_ea(xref.frm, db_filename))
+            else:
+                original_func_name = idc.get_func_name(f.start_ea)
+                demangled_func_name = idc.demangle_name(original_func_name, idc.INF_SHORT_DN)
+                func_name = demangled_func_name or original_func_name
+
+                node = {
+                    "project": "IDA: " + db_filename,
+                    "baseName": func_name,
+                    "baseAddress": str(f.start_ea)
+                }
+                if not self.line_xrefs:
+                    node['address'] = str(f.start_ea)
+                    node['computedProperties'] = [
+                        {
+                            "name": "label",
+                            "format": "{0}",
+                            "replacements": ["baseName"]
+                        }
+                    ]
+                else:
+                    node['address'] = str(xref.frm)
+                    node['line'] = '{0:x}'.format(xref.frm - f.start_ea)
+                    node['computedProperties'] = [
+                        {
+                            "name": "label",
+                            "format": "{0}+{1}",
+                            "replacements": ["baseName", "line"]
+                        }
+                    ]
+                nodes.append(node)
+        
+        if nodes:
+            return { "type": "addDataBulk", "nodes": nodes, "direction": "n2e" }
+        
+def raw_node_from_ea(ea, db_filename):
+    seg = ida_segment.getseg(ea)
+    if seg:
+        seg_name =  ida_segment.get_segm_name(seg) + ":"
+    else:
+        seg_name = ""
+    
+    name = idc.get_name(ea)
+    if not name or name == idc.BADADDR:
+        name = '{0:x}'.format(ea)
+
+    return {
+        "project": "IDA: " + db_filename,
+        "baseAddress": str(ea),
+        "address": str(ea),
+        "line": '{0:x}'.format(ea),
+        "baseName": name,
+        "seg": seg_name,
+        "computedProperties": [
             {
                 "name": "label",
-                "format": "{0}+{1}",
-                "replacements": ["function", "line"]
+                "format": "{0}{1}",
+                "replacements": ["seg", "baseName"]
             }
         ]
-        base['node']['address'] = str(ctx.cur_ea)
-        base['node']['line'] = '{0:x}'.format(ctx.cur_ea - ctx.cur_func.start_ea)
-        return base
+    }
+        
+class AddXrefLinesHandler(AddXrefsHandler):
+    def __init__(self):
+        AddXrefsHandler.__init__(self, True)
+
     
 class EnableSyncHandler(idaapi.action_handler_t):
     def __init__(self):
@@ -180,6 +280,21 @@ add_line_to_graph_action_desc = idaapi.action_desc_t(
     'Ctrl+Alt+A',      # Optional: the action shortcut
     'Add line to graph'
     )
+add_xrefs_to_graph_action_desc = idaapi.action_desc_t(
+    'graffiti:addXrefsToGraph', 
+    'Graffiti: Add function xrefs to graph',  # The action text.
+    AddXrefsHandler(),   # The action handler.
+    'Ctrl+Shift+Q',      # Optional: the action shortcut
+    'Add function xrefs to graph'
+    )
+add_xrefs_lines_to_graph_action_desc = idaapi.action_desc_t(
+    'graffiti:addXrefLinesToGraph', 
+    'Graffiti: Add line xrefs to graph',  # The action text.
+    AddXrefLinesHandler(),   # The action handler.
+    'Ctrl+Alt+Shift+Q',      # Optional: the action shortcut
+    'Add line xrefs to graph'
+    )
+
 enable_graffiti_sync_action_desc = idaapi.action_desc_t(
     'graffiti:ConnectToServer', 
     'Graffiti: Connect to server',  # The action text.
@@ -195,6 +310,10 @@ idaapi.unregister_action('graffiti:addToGraphWithEdgeInfo')
 idaapi.register_action(add_to_graph_with_edge_info_action_desc)
 idaapi.unregister_action('graffiti:addLineToGraph')
 idaapi.register_action(add_line_to_graph_action_desc)
+idaapi.unregister_action('graffiti:addXrefsToGraph')
+idaapi.register_action(add_xrefs_to_graph_action_desc)
+idaapi.unregister_action('graffiti:addXrefLinesToGraph')
+idaapi.register_action(add_xrefs_lines_to_graph_action_desc)
 idaapi.unregister_action('graffiti:ConnectToServer')
 idaapi.register_action(enable_graffiti_sync_action_desc)
 
@@ -210,6 +329,8 @@ class GraffitiUIHooks(idaapi.UI_Hooks):
             idaapi.attach_action_to_popup(form, popup, "graffiti:addToGraph", "Graffiti/")
             idaapi.attach_action_to_popup(form, popup, "graffiti:addToGraphWithEdgeInfo", "Graffiti/")
             idaapi.attach_action_to_popup(form, popup, "graffiti:addLineToGraph", "Graffiti/")
+            idaapi.attach_action_to_popup(form, popup, "graffiti:addXrefsToGraph", "Graffiti/")
+            idaapi.attach_action_to_popup(form, popup, "graffiti:addXrefLinesToGraph", "Graffiti/")            
 
 class GrafitiIDBHooks(idaapi.IDB_Hooks):
     def renamed(self, ea, new_name, is_local):
@@ -217,9 +338,9 @@ class GrafitiIDBHooks(idaapi.IDB_Hooks):
         if sock is not None and new_name:
             payload = {
                 "type": "updateNodes",
-                "selection": [["functionAddress", str(ea)]],
+                "selection": [["baseAddress", str(ea)]],
                 "update": {
-                    "function": new_name
+                    "baseName": new_name
                 }
             }
             lengthy_send(sock, to_bytes(json.dumps(payload)))
