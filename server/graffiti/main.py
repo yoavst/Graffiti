1256c799a102024e3d18f77cd7c33a3946792980
Yoav Sternberg <yoav.sternberg@gmail.com>
2024-05-15 20:45:32 +0300

    :sparkles: TLS support for frontend ws

--- server/graffiti/main.py
+++ server/graffiti/main.py
@@ -1,9 +1,12 @@
 import asyncio
 import logging
+import os
 import sys
-from typing import AsyncIterable
+from typing import Any, AsyncContextManager, AsyncIterable, Callable, Coroutine, Optional
 import websockets
+from websockets.server import WebSocketServerProtocol
 import argparse
+import ssl
 
 from .authenticator import Authenticator
 from .dispatcher import Dispatcher
@@ -27,12 +30,19 @@ async def get_stdin_lines() -> AsyncIterable[str]:
         yield line
 
 
-def port_type(astr, min=1, max=65536):
+def port_type(astr: str, min: int = 1, max: int = 65536) -> int:
     value = int(astr)
     if min <= value <= max:
         return value
     else:
         raise argparse.ArgumentTypeError(f"value not in range [{min}-{max}]")
+
+
+def file_type(astr: str) -> str:
+    if os.path.exists(astr):
+        return astr
+    else:
+        raise argparse.ArgumentTypeError("The provided path does not exist")
 
 
 def handle_args(args):
@@ -46,6 +56,18 @@ def handle_args(args):
         dump_messages = True
     if args.multi_user_mode:
         multi_user_mode = True
+
+
+def serve_websocket(
+    callback: Callable[[WebSocketServerProtocol], Coroutine[Any, Any, None]],
+    bind_ip: str,
+    bind_port: int,
+    ssl_context: Optional[ssl.SSLContext] = None,
+) -> AsyncContextManager:
+    if ssl_context is not None:
+        return websockets.serve(callback, bind_ip, bind_port, ssl=ssl_context)
+    else:
+        return websockets.serve(callback, bind_ip, bind_port)
 
 
 async def main():
@@ -73,6 +95,14 @@ async def main():
         metavar="[1-65535]",
     )
     parser.add_argument(
+        "--frontend-tls-context",
+        help="Chain and leaf to support WSS for frontend connection",
+        default=None,
+        nargs=2,
+        type=file_type,
+        metavar=("chain.crt", "leaf.key"),
+    )
+    parser.add_argument(
         "--multi-user-mode",
         help="Support multiple users at the same time by authenticating with uuid",
         action="store_true",
@@ -84,12 +114,18 @@ async def main():
     authenticator = Authenticator(dispatcher, args.multi_user_mode)
     adapter = NetworkHandlerAdapter(authenticator)
 
+    ssl_context = None
+    if args.frontend_tls_context is not None:
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        chain, key = args.frontend_tls_context
+        ssl_context.load_cert_chain(chain, key)
+
     print(
         f"Serving - frontend at {args.frontend_port} , TCP backend at {args.backend_tcp_port} , Websocket backend at {args.backend_websocket_port}"
     )
-    async with websockets.serve(adapter.handle_frontend_ws, "0.0.0.0", args.frontend_port):
+    async with serve_websocket(adapter.handle_frontend_ws, "0.0.0.0", args.frontend_port, ssl_context):
         async with await asyncio.start_server(adapter.handle_backend_tcp, "0.0.0.0", args.backend_tcp_port):
-            async with websockets.serve(adapter.handle_backend_ws, "0.0.0.0", args.backend_websocket_port):
+            async with serve_websocket(adapter.handle_backend_ws, "0.0.0.0", args.backend_websocket_port):
                 async for _ in get_stdin_lines():
                     print("Current status:")
                     print(repr(dispatcher))
