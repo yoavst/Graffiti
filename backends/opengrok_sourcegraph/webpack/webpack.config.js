7c4f959d390a0617b5cbd569ec3295518aa344b9
Yoav Sternberg <yoav.sternberg@gmail.com>
2024-05-24 16:07:45 +0300

    :construction_worker: Remove unnecessary config in webpack

--- backends/opengrok_sourcegraph/webpack/webpack.config.js
+++ backends/opengrok_sourcegraph/webpack/webpack.config.js
@@ -6,10 +6,8 @@ module.exports = {
     mode: "production",
     entry: {
         background: path.resolve(__dirname, "..", "src", "scripts", "background.ts"),
-        shared: path.resolve(__dirname, "..", "src", "scripts", "shared.ts"),
         content: path.resolve(__dirname, "..", "src", "scripts", "content.ts"),
         popup: path.resolve(__dirname, "..", "src", "popup", "popup.ts"),
-        opengrok: path.resolve(__dirname, "..", "src", "scripts", "opengrok.ts"),
     },
     output: {
         path: path.join(__dirname, "../dist"),
