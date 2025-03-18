d3b925d8a4a3a72b5be2b8cc225ec728df5cc792
Yoav Sternberg <yoav.sternberg@gmail.com>
2025-03-18 20:20:09 +0200

    :arrow_up: Update intellij platform plugin

--- backends/intellij/kotlin/build.gradle.kts
+++ backends/intellij/kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-    id("org.jetbrains.intellij.platform.base") version "2.1.0"
+    id("org.jetbrains.intellij.platform.base") version "2.3.0"
     id("java-library")
 }
 
--- backends/intellij/plugin/build.gradle.kts
+++ backends/intellij/plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-    id("org.jetbrains.intellij.platform") version "2.1.0"
+    id("org.jetbrains.intellij.platform") version "2.3.0"
     java
 }
 
